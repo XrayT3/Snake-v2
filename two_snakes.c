bf07bca6843a2459a81beb12f65a2bf61086d657
XrayT3 <sha0006@yandex.ru>
2021-05-25 20:05:44 +0200

    Add files via upload

--- two_snakes.c
+++ two_snakes.c
@@ -70,10 +70,10 @@ void moveSnakeAITwoSnakes(snake_t *snake, snake_t *secondSnake, cell_t *food, de
 
 int checkWallsCollisions(snake_t *snake, desk_t *desk) {
     if (
-        snake->snake_skeleton[0].x == 1  ||
-        snake->snake_skeleton[0].x == desk->width - 1 ||
-        snake->snake_skeleton[0].y == 1  ||
-        snake->snake_skeleton[0].y == desk->height - 1
+        snake->snake_skeleton[0].x == 0  ||
+        snake->snake_skeleton[0].x == desk->width ||
+        snake->snake_skeleton[0].y == 0  ||
+        snake->snake_skeleton[0].y == desk->height 
     )   return 1;
     return 0;
 }
@@ -102,28 +102,28 @@ int obstacleBeforeSnakeTwoSnakes(snake_t *snake, snake_t *otherSnake, desk_t *de
     int ret = 0;
     if (
         snake->direction == UP &&
-        snake->snake_skeleton[0].y == 2
+        snake->snake_skeleton[0].y == 1
     ) {
         ret = 1;
         return ret;
     }
     else if (
         snake->direction == DOWN &&
-        snake->snake_skeleton[0].y == desk->height - 2
+        snake->snake_skeleton[0].y == desk->height - 1
     ) {
         ret = 1;
         return ret;
     }
     else if (
         snake->direction == LEFT &&
-        snake->snake_skeleton[0].x == 2
+        snake->snake_skeleton[0].x == 1
     ) {
         ret = 1;
         return ret;
     }
     else if (
         snake->direction == RIGHT &&
-        snake->snake_skeleton[0].x == desk->width - 2
+        snake->snake_skeleton[0].x == desk->width - 1
     ) {
         ret = 1;
         return ret;
